f4c5f747dc6147371d102f84dc5cb932a040fac3
iqbalfachry19 <ifachry@rocketmail.com>
2022-02-19 13:16:00 +0700

    hi

---
+++ client/lib/constants.js
@@ -0,0 +1,20 @@
+import ProfileImageMinterContractAbi from "./ProfileImageNfts.json";
+
+export const contractABI = ProfileImageMinterContractAbi.abi;
+export const contractAddress = "0x899E089a1bEB74042fa192609d3DAfAeE5781FbB ";
+
+export const customStyles = {
+  content: {
+    top: "30%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "",
+    padding: 0,
+    border: "none",
+  },
+  overlay: {
+    backgroundColor: "#334250a7",
+  },
+};
